117664c0feafbe7068a342d9738890c6e1a92861
oladslw <oladayo.salawu@btcserv.co.uk>
2019-02-24 23:09:32 +0000

    [liquibase] xnm. liquibase

--- xnmdatabase/createXnmDBSchemaObjects.sh
+++ xnmdatabase/createXnmDBSchemaObjects.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 
-sudo docker cp create-xnm-pdb.sql oracle-xe:/opt/oracle/scripts/create-xnm-pdb.sql
+
+sudo docker cp src/main/resources/changelogs/sql/initial/db.changelog.intial.sql.create-xnmpdb.sql oracle-xe:/opt/oracle/scripts/create-xnmpdb.sql
 
 sudo docker cp src/main/resources/changelogs/sql/initial/db.changelog.initial.sql.create-xnmpdb-tablespace.sql oracle-xe:/opt/oracle/scripts/create-xnmpdb-tablespace.sql
 
 sudo docker cp src/main/resources/changelogs/sql/initial/db.changelog.initial.sql.update-xnmpdbadmin-account.sql oracle-xe:/opt/oracle/scripts/update-xnmpdbadmin-account.sql
 
-sudo docker cp src/main/resources/changelogs/sql/master-data/db.changelog.initial.sql.create-liquibase-schema.sql oracle-xe:/opt/oracle/scripts/db.changelog.initial.sql.create-liquibase-schema.sql
+sudo docker cp src/main/resources/changelogs/sql/master-data/db.changelog.initial.sql.create-liquibase-schema.sql oracle-xe:/opt/oracle/scripts/create-liquibase-schema.sql
 
 sudo docker exec -it oracle-xe bash -c "source /home/oracle/.bashrc; bash"
 
 #RUN $ORACLE_HOME/bin/sqlplus sys/Oracle18@localhost/XE as sysdba
 
-#@/opt/oracle/scripts/create-xnm-pdb.sql
+#@/opt/oracle/scripts/create-xnmpdb.sql
+
+#@/opt/oracle/scripts/create-xnmpdb-tablespace.sql
+
+#@/opt/oracle/scripts/update-xnmpdbadmin-account.sql
+
+#@/opt/oracle/scripts/create-liquibase-schema.sql
