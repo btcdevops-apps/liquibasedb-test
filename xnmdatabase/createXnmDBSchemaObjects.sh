a02dffa74f8c4a6061d35b922d0b48dbc74d5617
oladslw <oladayo.salawu@btcserv.co.uk>
2019-11-20 00:01:21 +0000

    updated xnm_numeric_info

--- xnmdatabase/createXnmDBSchemaObjects.sh
+++ xnmdatabase/createXnmDBSchemaObjects.sh
@@ -9,6 +9,8 @@ sudo docker cp src/main/resources/changelogs/sql/initial/db.changelog.initial.sq
 
 sudo docker cp src/main/resources/changelogs/sql/master-data/db.changelog.initial.sql.create-liquibase-schema.sql oracle-xe:/opt/oracle/scripts/create-liquibase-schema.sql
 
+sudo docker exec -it oracle-xe bash -c "source /home/oracle/.bashrc; bash" && $ORACLE_HOME/bin/sqlplus sys/Oracle18@localhost/XE as sysdba
+
 sudo docker exec -it oracle-xe bash -c "source /home/oracle/.bashrc; bash"
 
 $ORACLE_HOME/bin/sqlplus sys/Oracle18@localhost/XE as sysdba
